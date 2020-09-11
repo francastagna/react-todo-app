179819b5c78ae3872e0b8282b24944d8612ffe33
Franco Nicolas Castagna <fcastagna@dc.uba.ar>
2020-09-11 01:57:30 -0300

    Updates

---
+++ precache-manifest.f769031decf16c7eb418d28afb3c2180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab4eb60ebe2611e8c9cdad3aa873ce9",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "d9c4607643052a239ecf",
+    "url": "/react-todo-app/static/css/main.bcff6208.chunk.css"
+  },
+  {
+    "revision": "8cb620173de546cd7e16",
+    "url": "/react-todo-app/static/js/2.b731b154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.b731b154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c4607643052a239ecf",
+    "url": "/react-todo-app/static/js/main.c1c02346.chunk.js"
+  },
+  {
+    "revision": "a02306a5582400e4c981",
+    "url": "/react-todo-app/static/js/runtime-main.15b7a15e.js"
+  }
+]);
